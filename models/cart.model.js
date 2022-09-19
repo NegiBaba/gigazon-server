fcc383285663e00be7e2b262a77cf761fd43d329
NegiBaba <piyushnegi319@gmail.com>
2022-09-19 20:01:41 +0530

    Added cart route and controller

--- models/cart.model.js
+++ models/cart.model.js
@@ -2,8 +2,8 @@ import mongoose, { Schema } from 'mongoose';
 import { productSchema } from './product.model';
 
 const cartSchema = new Schema({
-  count: Number,
-  products: [productSchema]
+  product: productSchema,
+  quantity: Number
 })
 
 export default mongoose.model('cart', cartSchema);
