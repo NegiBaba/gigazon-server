89ce536d4b1b0fe3ef1acaf1fd0fbc93504117e6
NegiBaba <piyushnegi319@gmail.com>
2022-09-22 01:57:46 +0530

    Added condition for increment and decrement quantity

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,14 +2,16 @@ import Cart from '../models/cart.model'
 
 const upsert = async (req, res) => {
   const { product, quantity } = req.body;
-  const foundProduct = await Cart.findOne().where('product._id').equals(product._id);
+  const foundProduct = await Cart.findOne({
+    'product._id': product.product._id
+  })
   if (foundProduct === null) {
     const productToBeAdded = new Cart({
       product,
       quantity
     });
     try {
-      const prod = await productToBeAdded.save();
+      await productToBeAdded.save();
       return res.status(200).json({
         message: 'Successfully added product to the cart!'
       })
@@ -19,15 +21,18 @@ const upsert = async (req, res) => {
       })
     }
   } else {
+    foundProduct.quantity = quantity;
+    await foundProduct.save();
     return res.status(200).json({
-      message: "Product already in cart!"
+      message: "Succesfully updated product's quantity",
+      product: foundProduct
     })
   }
 }
 
 const list = async (req, res) => {
   try {
-    const products = await Cart.find({}, { product: 1 });
+    const products = await Cart.find({});
     return res.json(products);
   } catch (error) {
     console.log(error)
