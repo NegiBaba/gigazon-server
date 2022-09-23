ef04442e7ad06ad0f7250d8d845321c85dd8de1a
NegiBaba <piyushnegi319@gmail.com>
2022-09-24 00:35:06 +0530

    Added delete route for the cart

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -3,6 +3,6 @@ import cartController from "../controllers/cart.controller"
 
 const router = new Router();
 
-router.route('/api/cart').get(cartController.list).post(cartController.upsert)
+router.route('/api/cart').get(cartController.list).post(cartController.upsert).delete(cartController.remove)
 
 export default router;
