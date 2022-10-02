1f11761d55c63f5308867fb317f5bb4be2b60654
NegiBaba <piyushnegi319@gmail.com>
2022-10-02 23:41:49 +0530

    Removed logs

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,4 @@
-import Product from '../models/product.model'
+import Product from '../models/product.model';
 
 /**
  * Adds a new product to the database, to be used only by developer that is why doesn't contains req, res params
@@ -7,37 +7,35 @@ import Product from '../models/product.model'
 const addProduct = async (req, res) => {
   const product = new Product(req.body);
   try {
-    await product.save()
+    await product.save();
     return res.status(200).json({
       message: 'Succesfully added new product',
-      ...req.body
+      ...req.body,
     });
   } catch (error) {
     console.log('error adding a new product ', error);
   }
-}
+};
 
 /**
  * Async method to fetch list of products
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  * @returns list of product
  */
 const list = async (req, res) => {
   try {
     const products = await Product.find({});
     return res.json(products);
-  }
-  catch (error) {
+  } catch (error) {
     console.log('error fetching list of products ', error);
     res.status(400).json({
-      error: error
-    })
+      error: error,
+    });
   }
-}
+};
 
 const update = async (req, res) => {
-  console.log(req.body)
   const { _id, field, value } = req.body;
   try {
     const product = await Product.findOne({ _id: _id });
@@ -46,22 +44,21 @@ const update = async (req, res) => {
       try {
         await product.save();
         res.status(200).json({
-          message: 'Successfully updated the product!'
-        })
+          message: 'Successfully updated the product!',
+        });
       } catch (error) {
         res.status(400).json({
-          error: error
-        })
+          error: error,
+        });
       }
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
 export default {
   addProduct,
   list,
-  update
-}
+  update,
+};
